c21711c67c9c70c1ddba8e712df6829177a836b5
syahirahriadz <aidasyahirahriadz@gmail.com>
2025-08-07 23:07:12 +0800

    kelas2

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/tentang-blog', function () {
+    return view('tentang-blog');
+})->name('about');
